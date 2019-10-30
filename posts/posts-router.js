05622d3dccbb6b58e73c6c97dba200a1b47dda56
sooshe <sooshe@sooshebot.com>
2019-10-30 16:45:02 -0500

    add post to posts-router

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -3,7 +3,31 @@ const router = express.Router();
 const Posts = require('../data/db');
 router.use(express.json());
 
+// POST /api/posts -- saves and returns new post to the array
+router.post('/', (req,res) => {
+  const { title, contents } = req.body;
 
+  Posts.insert(req.body)
+  .then(posts => {
+    if (posts) {
+      res.status(201).json(posts)
+    } else {
+      res
+          .status(500)
+          .json({
+            error: "There was an error while saving this post to the database."
+          });
+    }
+  })
+  .catch(error => {
+      console.log("An error, alas!", error);
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide the following-- 'title':'', 'contents':'' for this post." });
+    });
+});
+
+// GET /api/posts -- finds and returns the posts array
 router.get('/', (req, res) => {
     const query = req.query;
   
@@ -19,6 +43,7 @@ router.get('/', (req, res) => {
       });
   });
 
+  // get /api/posts/:id -- finds and returns posts by ID
   router.get(() => {
 
   });
