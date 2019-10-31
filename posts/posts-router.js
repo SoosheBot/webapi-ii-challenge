96c9c666dfd7db461ebf68b53bc9fbba595f8ad7
sooshe <sooshe@sooshebot.com>
2019-10-30 19:14:16 -0500

    strange bug in getbyid call

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,7 +4,7 @@ const Posts = require("../data/db");
 router.use(express.json());
 
 
-// GET /api/posts -- finds and returns the posts array
+// GET /api/posts 
 router.get("/", (req, res) => {
   const query = req.query;
 
@@ -20,24 +20,42 @@ router.get("/", (req, res) => {
     });
 });
 
-// GET /api/posts/:id -- finds and returns posts by ID
+// GET /api/posts/:id 
 router.get("/:id", (req,res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   Posts.findById(id)
     .then(posts => {
-      if (id > posts.length) {
-        res.status(404).json({message: "The post with the specified ID does not exist."});
+      if (posts) {
+        res.status(200).json(posts);   
       } else {
-        res.status(200).json(posts);
+        res.status(404).json({message: "The post with the specified ID does not exist."});
       }
     })
     .catch(error => {
       console.log("An error, alas!", error);
       res
         .status(500)
-        .json({ error: "The contents could not be retrieved." });
+        .json({ error: "The post information could not be retrieved." });
     });
 });
+
+// GET /api/posts/:id/comments
+// router.get(() => {
+//   const { id } = req.params;
+//   Posts.findCommentById(id)
+//   .then(posts => {
+//     if (posts !== id) {
+//       res.status(404).json({message: "The post with the specified ID does not exist."});
+//     } 
+//       res.status(200).json(posts);
+//   })
+//   .catch(error => {
+//     console.log("An error, alas!", error);
+//     res
+//       .status(500)
+//       .json({ error: "The contents could not be retrieved." });
+//   });
+// });
 
 
 
