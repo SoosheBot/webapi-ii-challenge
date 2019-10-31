3023577767b125a6571ea4aebd081f8855114d12
sooshe <sooshe@sooshebot.com>
2019-10-30 20:53:24 -0500

    fix get and post bugs in posts-router

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -6,9 +6,9 @@ router.use(express.json());
 
 // GET all posts -- /api/posts 
 router.get("/", (req, res) => {
-  const query = req.query;
+  const body = req.body;
 
-  Posts.find(query)
+  Posts.find(body)
     .then(posts => {
       res.status(200).json(posts);
     })
@@ -25,7 +25,7 @@ router.get("/:id", (req,res) => {
   const {id} = req.params;
   Posts.findById(id)
     .then(posts => {
-      if (posts) {
+      if (posts.length) {
         res.status(200).json(posts);   
       } else {
         res.status(404).json({message: "The post with the specified ID does not exist."});
@@ -42,12 +42,13 @@ router.get("/:id", (req,res) => {
 // GET comment -- /api/posts/:id/comments
 router.get("/:id/comments", (req,res) => {
   const { id } = req.params;
-  Posts.findCommentById(id)
+  Posts.findPostComments(id)
   .then(posts => {
-    if (posts) {
-      res.status(200).json(posts); 
-    } 
-      res.status(404).json({message: "The post with the specified ID does not exist."});
+    if (posts.length <= 0) {
+      res.status(404).json({message: "The post with the specified ID does not exist."});   
+    } else {
+      res.status(201).json(posts);
+    }
   })
   .catch(error => {
     console.log("An error, alas!", error);
@@ -57,6 +58,50 @@ router.get("/:id/comments", (req,res) => {
   });
 });
 
+// POST to array -- /api/posts
+router.post("/", (req, res) => {
+  const {title, contents } = req.body;
 
+  Posts.insert(req.body)
+    .then(posts => {
+      if (req.body === '') {
+        res.status(400).json({errorMessage:'Please provide title and contents to make this work.'})
+      } else {
+        res.status(201).json(posts);
+      } 
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "There was an error saving this post, try again."
+      });
+    });
+});
+
+//POST create comment for specific post by ID -- /api/posts/:id/comments
+router.post("/:id/comments", (req, res) => {
+  const {id} = req.params;
+  const {text} = req.body;
+  
+  const newComment = {
+    ...req.body,
+    post_id:id
+  }
+
+  Posts.insertComment(newComment)
+  .then(posts => {
+    res.status(200).json(posts);  
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "There was an error saving this post, try again."
+    });
+  });
+});
+  
+
+
+  
 
 module.exports = router;
